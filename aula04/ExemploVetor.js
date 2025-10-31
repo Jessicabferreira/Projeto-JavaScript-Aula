be8f227dc82c5fedc7152c9a1668c2eb2a685a54
Jessica Berteloni Ferreira <jessiberteloni@gmail.com>
2025-10-31 14:22:25 -0300

    Exemplos de Vetores

---
+++ aula04/ExemploVetor.js
@@ -0,0 +1,7 @@
+let vetorStrings = ["Boxer", "Pastor Alemão", "Pinscher", "Husky Siberiano", "Corgi"];
+
+for (let indice = 0; indice < 5; indice++)
+  {
+      console.log(`${indice + 1}º elemento: ${vetorStrings[indice]}`);
+  }
+  console.log(vetorStrings.length) // comprimento tamanho do vetor
