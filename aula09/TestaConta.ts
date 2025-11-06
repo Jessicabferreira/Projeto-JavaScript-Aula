61a9605b66cdcb483df36731f04912fe31a9409c
Jessica <jessiberteloni@gmail.com>
2025-11-06 13:25:26 -0300

    Classes, Objetos e Encapsulamento

---
+++ aula09/TestaConta.ts
@@ -0,0 +1,18 @@
+import { Conta } from "./Conta"
+
+export function main() {
+
+// INSTANCIA DA CLASSE CONTA
+// INSTANCIA DE UM OBJETO
+const conta1: Conta = new Conta(12156, 108, 1, "Michelle", 10000000) 
+
+console.log("Titular da Conta:" + conta1.titular)
+
+conta1.saldo = 15000000
+
+console.log("Saldo da conta:" + conta1.saldo)
+
+
+}
+
+main()
