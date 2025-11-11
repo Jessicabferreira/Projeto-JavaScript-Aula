540578ea2458a3b88a5415a7ab937caea210ec6a
Jessica <jessiberteloni@gmail.com>
2025-11-11 11:07:09 -0300

    Tratamento de erros no TypeScript - Exceptions

---
+++ aula13/Exemplo1.ts
@@ -0,0 +1,23 @@
+import { converte } from "./Funcoes";
+
+export function main() {
+
+    try{ 
+
+        const resultado = converte(123);
+        console.log(resultado);
+
+    }catch(error){     // Captura o possível erro
+
+        if(error instanceof TypeError) { // Verifica o tipo de erro
+            console.error("TypeError: " + error.message)
+            console.log("Usuário, está havendo um erro nessa função relacionado ao tipo do conteúdo. Por favor insira um valor textual")
+        }
+
+    }finally {
+        console.log("Sempre irei executar!")
+    }
+
+}
+
+main()
